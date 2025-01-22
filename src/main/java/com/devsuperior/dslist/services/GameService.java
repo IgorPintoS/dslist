82917fda2d82333904a2939648798d600987a7fa
IgorPintoS <igorpintosantos05@gmail.com>
2025-01-22 20:36:22 -0300

    GameDTO

--- src/main/java/com/devsuperior/dslist/services/GameService.java
+++ src/main/java/com/devsuperior/dslist/services/GameService.java
@@ -1,12 +1,16 @@
 package com.devsuperior.dslist.services;
 
+import com.devsuperior.dslist.DTO.GameDTO;
 import com.devsuperior.dslist.DTO.GameMinDTO;
 import com.devsuperior.dslist.entities.Game;
 import com.devsuperior.dslist.repositories.GameRepository;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class GameService {
@@ -14,9 +18,21 @@ public class GameService {
     @Autowired
     private GameRepository gameRepository;
 
+    @Transactional(readOnly = true)
     public List<GameMinDTO> findAll(){
         List<Game> result = gameRepository.findAll();
         List<GameMinDTO> dto = result.stream().map(x -> new GameMinDTO(x)).toList();
         return dto;
+    }
+    @Transactional(readOnly = true)
+    public GameDTO findById(Long id) {
+        Optional<Game> gameOptional = gameRepository.findById(id);
+
+        if (gameOptional.isPresent()) {
+            Game result = gameOptional.get();
+            return new GameDTO(result);
+        } else {
+            throw new EntityNotFoundException("Jogo com o id: " + id + " não encontrado.");
+        }
     }
 }
