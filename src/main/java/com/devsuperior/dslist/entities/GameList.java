ea8825ff55ee45dcc175ea01601efd9f078d188f
IgorPintoS <igorpintosantos05@gmail.com>
2025-01-22 21:19:57 -0300

    Get Lists

--- src/main/java/com/devsuperior/dslist/entities/GameList.java
+++ src/main/java/com/devsuperior/dslist/entities/GameList.java
@@ -21,6 +21,22 @@ public class GameList {
         this.name = nome;
     }
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
---
+++ src/main/java/com/devsuperior/dslist/services/GameListService.java
@@ -0,0 +1,29 @@
+package com.devsuperior.dslist.services;
+
+import com.devsuperior.dslist.DTO.GameDTO;
+import com.devsuperior.dslist.DTO.GameListDTO;
+import com.devsuperior.dslist.DTO.GameMinDTO;
+import com.devsuperior.dslist.entities.Game;
+import com.devsuperior.dslist.entities.GameList;
+import com.devsuperior.dslist.repositories.GameListRepository;
+import com.devsuperior.dslist.repositories.GameRepository;
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class GameListService {
+
+    @Autowired
+    private GameListRepository gameListRepository;
+
+    @Transactional(readOnly = true)
+    public List<GameListDTO> findAll(){
+        List<GameList> result = gameListRepository.findAll();
+        return result.stream().map(x -> new GameListDTO(x)).toList();
+    }
+}
